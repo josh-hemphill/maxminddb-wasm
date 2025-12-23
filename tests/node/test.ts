1293909f97dd2b4a2b5bf580347b40171c2b8e64
jj <log@riseup.net>
2025-12-23 15:50:12 +0000

    lib: add support for IspResponse
    Closes #8

--- tests/node/test.ts
+++ tests/node/test.ts
@@ -5,6 +5,7 @@ import { readFileSync } from 'node:fs'
 import * as _Maxmind from '../../node/index.js'
 
 const dbFile = readFileSync(join(__dirname, '..', '.GeoLite2-City-Test.mmdb'))
+const dbFile_AS = readFileSync(join(__dirname, '..', '.GeoLite2-ASN-Test.mmdb'))
 
 
 describe('Maxmind DB', () => {
@@ -15,6 +16,21 @@ describe('Maxmind DB', () => {
 	it('should return the correct result', () => {
 		expect(result).toBeDefined()
 		expect(result?.location?.time_zone).toBe("Europe/Warsaw")
+	})
+	it('db should have the correct metadata', () => {
+		expect(maxmind?.metadata?.languages?.includes('en')).toBe(true)
+	})
+})
+
+describe('Maxmind DB ASN', () => {
+	const maxmind = new _Maxmind.Maxmind(dbFile_AS)
+
+	const result = maxmind.lookup_isp('2c0f:ff80::')
+
+	it('should return the correct result', () => {
+		expect(result).toBeDefined()
+		expect(result.asn?.as_num).toBe(237)
+		expect(result.asn?.as_organization).toBe("Merit Network Inc.")
 	})
 	it('db should have the correct metadata', () => {
 		expect(maxmind?.metadata?.languages?.includes('en')).toBe(true)
